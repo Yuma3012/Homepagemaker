ba61c6b088bd209f741114aa01c8d64d91a397ab
Yuma3012 <yumakobayashi1206@icloud.com>
2023-09-18 18:37:20 +0900

    #02

--- upload.php
+++ upload.php
@@ -1,4 +1,16 @@
 <?php
+
+$text_input = '';
+
+if (isset($_POST['input_text'])) {
+    $text_input = $_POST['input_text'];
+
+    // 処理が成功した場合、result.html を表示
+    $template = file_get_contents('result.html');
+    $text = str_replace('{TEXT}', !empty($img_path) ? $img_path : '', $template);
+    echo $image;
+}
+
 if (!empty($_FILES)) {
     // images_after フォルダ内のすべてのファイルを削除
     $files = glob('images_after/*');
@@ -20,25 +32,28 @@ if (!empty($_FILES)) {
             // 新しい画像のパスを設定
             $img_path = $uploaded_path;
             $MSG = 'アップロード成功！ファイル名：' . $filename;
+
+            // 処理が成功した場合、result.html を表示
+            $template = file_get_contents('result.html');
+            $image = str_replace('{IMG_SRC}', !empty($img_path) ? $img_path : '', $template);
+            echo $image;
         } else {
-            $MSG = 'アップロード失敗！エラーコード：' . $_FILES['upload_image']['error'];
+            // 処理が失敗した場合、upload.html を再表示
+            include('upload.html');
         }
-        $template = file_get_contents('copy/index.html');
-        $template = str_replace('{IMG_SRC}', !empty($img_path) ? $img_path : '', $template);
-        echo $template;
     } else {
         // 画像ファイルでない場合の処理
+        // $template = file_get_contents('upload.html');
         $MSG = '画像ファイルではありません';
+        // upload.html を再表示
+        include('upload.html');
     }
 } else {
+    // $template = file_get_contents('upload.html');
     $MSG = '画像を選択してください';
-
+    
+    // 最初の画面（upload.html）を表示
+    include('upload.html');
 }
 
-// HTMLファイルの読み込み
-$template = file_get_contents('upload.html');
-// $template = str_replace('{IMG_SRC}', !empty($img_path) ? $img_path : '', $template);
-// // $template = str_replace('{MESSAGE}', !empty($MSG) ? $MSG : '', $template);
-
-echo $template;
 ?>
